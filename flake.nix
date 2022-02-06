5f7c19a51373558260bf4cf57687e3193fcc8d78
Sridhar Ratnakumar <srid@srid.ca>
2022-02-06 13:15:30 -0500

    tryu toplevel

--- flake.nix
+++ flake.nix
@@ -11,36 +11,37 @@
   };
 
   outputs = { self, flake-utils, emanote, nixpkgs, ... }@inputs:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = nixpkgs.legacyPackages.${system};
-      in
-      rec {
-        defaultApp = apps.live;
-        apps = {
-          live = rec {
-            type = "app";
-            # '' is required for escaping ${} in nix
-            script = pkgs.writeShellApplication {
-              name = "emanoteRun.sh";
-              text = ''
-                set -xe
-                export PORT="''${EMANOTE_PORT:-7072}"
-                ${emanote.defaultPackage.${system}}/bin/emanote run --port "$PORT"
-              '';
+    flake-utils.lib.eachDefaultSystem
+      (system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
+        rec {
+          defaultApp = apps.live;
+          apps = {
+            live = rec {
+              type = "app";
+              # '' is required for escaping ${} in nix
+              script = pkgs.writeShellApplication {
+                name = "emanoteRun.sh";
+                text = ''
+                  set -xe
+                  export PORT="''${EMANOTE_PORT:-7072}"
+                  ${emanote.defaultPackage.${system}}/bin/emanote run --port "$PORT"
+                '';
+              };
+              program = "${script}/bin/emanoteRun.sh";
             };
-            program = "${script}/bin/emanoteRun.sh";
           };
-        };
-        devShell = pkgs.mkShell {
-          buildInputs = [ pkgs.nixpkgs-fmt ];
-        };
-
-        # Hercules CI support: https://hercules-ci.com/
-        ciNix = args@{ src }: inputs.flake-compat-ci.lib.recurseIntoFlakeWith {
-          flake = self;
-          systems = [ system ];
-          effectsArgs = args;
-        };
-      });
+          devShell = pkgs.mkShell {
+            buildInputs = [ pkgs.nixpkgs-fmt ];
+          };
+        }) // {
+      # Hercules CI support: https://hercules-ci.com/
+      ciNix = args@{ src }: inputs.flake-compat-ci.lib.recurseIntoFlakeWith {
+        flake = self;
+        systems = [ "x86_64-linux" ];
+        effectsArgs = args;
+      };
+    };
 }
