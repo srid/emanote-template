d02c989a7c72d8bee7c5dde2e9f6c8f0110ab8bf
Sridhar Ratnakumar <srid@srid.ca>
2024-03-12 19:01:01 -0400

    Update emanote; along with new module

--- flake.nix
+++ flake.nix
@@ -20,8 +20,7 @@
           # By default, the 'emanote' flake input is used.
           # package = inputs.emanote.packages.${system}.default;
           sites."default" = {
-            layers = [ ./. ];
-            layersString = [ "." ];
+            layers = [{ path = ./.; pathString = "."; }];
             # port = 8080;
             baseUrl = "/emanote-template/"; # Change to "/" (or remove it entirely) if using CNAME
             # prettyUrls = true;
